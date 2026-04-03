31b4fcf31c1dfbd6c6b9531aa5db2b8137c60ecf
saswata221 <saswatamahato652@gmail.com>
2026-04-04 00:30:51 +0530

    Fixed some bug and updated API URL for production

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ const eggRoutes = require("./routes/eggRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 app.use(auth);
